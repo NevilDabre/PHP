b53e0182d5d1e1826f58098c7dd142c63782b296
Neville <nevil_dabre@yahoo.com>
2016-11-20 12:40:22 -0500

    Thirdupdate

--- validation.php
+++ validation.php
@@ -6,6 +6,7 @@ $server="localhost";
 $dbpass="";
 $dbuser="root";
 $dbname="testdb";
+$userflag=$passflag=0;
 
 $conn = new mysqli($server,$dbuser,$dbpass,$dbname);
 
@@ -14,50 +15,43 @@ if($conn->connect_error)
     echo "database not connected<br>";
 
 }
-else
-{
-    if($_SERVER["REQUEST_METHOD"]=="POST")
-    {
-        if(empty($_POST["email"])){
+else {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["email"])) {
 
-                echo "enter your user name<br>";
+            echo "enter your user name<br>";
+        } else {
+            $username = testdata($_POST["email"]);
+            if (!filter_var($username, FILTER_VALIDATE_EMAIL)) {
+                echo "enter valid user name.<br>";
+            } else {
+                $userflag = 1;
             }
-            else
-            {
-                $username=testdata($_POST["email"]);
-                if(!filter_var($username,FILTER_VALIDATE_EMAIL))
-                {
-                    echo "enter valid user name.<br>";
+        }
+
+        if (empty($_POST["password"])) {
+            echo("enter password.<br>");
+        } else {
+            $password = testdata($_POST["password"]);
+            $passflag = 1;
+        }
+
+        if ($passflag == 1 && $userflag == 1) {
+            $sql = "select * from test WHERE email='" . $username . "' and name ='" . $password . "'";
+            echo $sql;
+            $row = $conn->query($sql);
+            if ($row->num_rows > 0) {
+                while($result = $row->fetch_assoc()) {
+                if ($result["email"] == $username && $result["name"] == $password) {
+                    echo "Welcome" . $username . "<br>";
                 }
+                else {
+                    echo "wrong user credentials";
+                }
+            }} else {
+                echo "user not exist<br>";
             }
-
-        if (empty($_POST["password"]))
-        {
-         echo ("enter password.<br>");
         }
-        else
-            {
-               $password = testdata($_POST["password"]);
-            }
-    }
-
-    $sql = "select * from test WHERE email='".$username."' and name ='".$password."'";
-    echo $sql;
-    $row = $conn->query($sql);
-    if($row->num_rows>0)
-    {
-        if($row["email"]==$username && $row["name"]==$password)
-        {
-            echo "Welcome".$username."<br>";
-        }
-        else
-        {
-            echo "wrong credentials<br>";
-        }
-    }
-    else
-    {
-        echo "user not exist<br>";
     }
 }
 
